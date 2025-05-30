67269614cc8945c09173c44dab28ba5cc1315bc7
@do.duong <vanduong.do2000@gmail.com>
2025-05-30 07:48:30 +0700

    update shop and product detail

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./layout/header/header.component";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
