d76cc50e6c19fe2249df97341766571bae3caee8
@do.duong <vanduong.do2000@gmail.com>
2025-06-13 07:14:24 +0700

    update order controller

---
+++ API/DTOs/CreateOrderDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Core.Entities.OrderAggregate;
+
+namespace API.Dtos;
+
+public class CreateOrderDto
+{
+    [Required]
+    public string CartId { get; set; } = string.Empty;
+    [Required]
+    public int DeliveryMethod { get; set; }
+    [Required]
+    public ShippingAddress ShippingAddress { get; set; } = null!;
+    [Required]
+    public PaymentSummary PaymentSummary { get; set; } = null!;
+}
